e2bf6ae21571156a91a91ab73abbf3ba4d3c0fd5
maurice diong <mdiong00@gmail.com>
2023-12-26 08:07:50 -0500

    completed patch

--- src/index.js
+++ src/index.js
@@ -13,7 +13,12 @@ dogs.forEach(dog => {
     dogDiv.append(dogSpan) //append span into dogDiv
     dogSpan.textContent=dog.name //add dog name
     //add dog card
-    dogSpan.addEventListener('click',handleClick.bind(dog))
+    dogSpan.addEventListener('click',handleClick)
+
+    const img =document.createElement('img')
+    const h2 =document.createElement('h2')
+    const button =document.createElement('button')
+
     function handleClick(e){
         //grab the dog info container
         const dogInfo=document.querySelector('#dog-info')
@@ -21,16 +26,27 @@ dogs.forEach(dog => {
         while(dogInfo.firstChild){
         dogInfo.removeChild(dogInfo.firstChild)}
         //put together dog details
-        console.log(dog.name)
-        const img =document.createElement('img')
+        //console.log(dog.name)
         img.src=dog.image
-        const h2 =document.createElement('h2')
         h2.textContent=dog.name
-        const button =document.createElement('button')
-        button.textContent="Good Dog!"
+        button.textContent=(dog.isGoodDog?"Good Dog!":"Bad Dog!")
         
         dogInfo.append(img,h2,button)
     }
+    
+    button.addEventListener('click',toggle)
+
+    function toggle(){
+        fetch(`http://127.0.0.1:3000/pups/${dog.id}`,{
+            method:"PATCH",
+            headers:{'Content-Type':"application/JSON"},
+            body: JSON.stringify({isGoodDog:!dog.isGoodDog})
+        })
+        .then((r)=>r.json())
+        .then((data)=>console.log(data))
+
+    }
+
 });
 }
 
